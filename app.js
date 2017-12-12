b8764ffc1b38093119069dab10a849fe4243f03d
trumpetcoder <kevindjames55@gmail.com>
2017-12-12 15:43:15 -0700

    Added for squares and color reset working

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 // Linked app.js sheet to index.html
 // Checked and Working
 
-$('#game').click(function() {
+var clicks = 10;
+
+$('#cell1').click(function() {
 	$(this).css('background', 'yellow');
 });
 
-$('.reset').click(function() {
-	alert('You Clicked the reset button');
+$('#cell2').click(function() {
+	$(this).css('background', 'yellow');
 });
+
+$('#cell3').click(function() {
+	$(this).css('background', 'yellow');
+});
+		
+$('#cell4').click(function() { 
+	$(this).css('background', 'yellow');
+});
+
+
+
+
+
+	// $(this).css('background', 'yellow');
+
+// });
+
+$('.reset').click(function() {
+	$('.cell').css('background', 'green');
+});
+
+// $('#leftclick').html(clicks --);
+
+// $('#rightclick').html(clicks --);
+
+// $('#leftclick').html(clicks);
+
+// $('#reset').click(function() {
+//    	$('.cell').html("").css('background', 'white');
+//    	clickCount = 0;
+// });
