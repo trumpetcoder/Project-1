b27d4c444e0044d4ea9ab9ce783bd34098f7c0ea
trumpetcoder <kevindjames55@gmail.com>
2017-12-11 18:27:45 -0700

    Added + checked initial click functionality on board and button

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 // Linked app.js sheet to index.html
 // Checked and Working
 
+$('#game').click(function() {
+	alert('You Clicked the game board');
+});
+
+$('.reset').click(function() {
+	alert('You Clicked the reset button');
+});
