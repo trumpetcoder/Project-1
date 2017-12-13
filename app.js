1ffe3e81b8e7dea14555545390b72604bc1fba36
trumpetcoder <kevindjames55@gmail.com>
2017-12-13 00:52:55 -0700

    Tightened up, commented, and removed erroneous code in all files

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 // Linked app.js sheet to index.html
 // Checked and Working
 
-var clicks = 10;
 
 
-
+// Functionality for click function. Border and cell disappear when clicked
 $('.cell').click(function() {
 	$(this).css({'background': 'none', 'border': 'none'});
 	$('#obj').show();
 });
 
-
+// Functionality for reset button. Returns borders to initial state, object hidden()
 $('.reset').click(function() {
 	$('.cell').css({'background': '#6D1B49', 'border': '1px solid black'});
 	$('#obj').hide();
@@ -18,11 +17,16 @@ $('.reset').click(function() {
 });
 
 
-
+// Funtionality for hidden function
 function hidden () {
+
+	// setup variable for all cells
 	var cells = $('.cell');
+	// setup variable random with logic to choose any cell on board
 	var random = Math.floor(Math.random() * cells.length)+1;
+	// inserting treasure chest (#obj) into randomly selected cell
 	$('#obj').appendTo($('#cell' + random));
+	// checking in dev tools for random cell number and seeing if it matches
 	console.log(random);
 }
 
