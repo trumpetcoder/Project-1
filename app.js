b4bc9865f87ba7dcff2102b17654a9acd06e303f
trumpetcoder <kevindjames55@gmail.com>
2017-12-12 18:48:21 -0700

    added randomized obj placement

--- app.js
+++ app.js
@@ -3,32 +3,36 @@
 
 var clicks = 10;
 
+
+
 $('#cell1').click(function() {
-	$(this).css('background', 'yellow');
+	$(this).css('background', '#332200');
+	// $('#obj').show();
 });
 
 $('#cell2').click(function() {
-	$(this).css('background', 'yellow');
+	$(this).css('background', '#332200');
+	// $('#obj').show();
 });
 
 $('#cell3').click(function() {
-	$(this).css('background', 'yellow');
+	$(this).css('background', '#332200');
+	// $('#obj').show();
 });
 		
 $('#cell4').click(function() { 
-	$(this).css('background', 'yellow');
+	$(this).css('background', '#332200');
+	// $('#obj').show();
 });
 
 
 
 
 
-	// $(this).css('background', 'yellow');
-
-// });
-
 $('.reset').click(function() {
-	$('.cell').css('background', 'green');
+	$('.cell').css('background', '#805500');
+	$('#obj').hide();
+	hidden();
 });
 
 // $('#leftclick').html(clicks --);
@@ -41,3 +45,15 @@ $('.reset').click(function() {
 //    	$('.cell').html("").css('background', 'white');
 //    	clickCount = 0;
 // });
+
+function hidden () {
+	var cells = $('.cell');
+	var random = Math.floor(Math.random() * cells.length)+1;
+	$('#obj').appendTo($('#cell' + random));
+	console.log(random);
+}
+
+hidden();
+
+
+
